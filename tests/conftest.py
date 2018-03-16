bcea0a19552e33fa1f5575de8adf0e9d2c3a5fa7
Justin Barnowski <jbarno@uchicago.edu>
2018-03-16 13:45:25 -0500

    fix(imports): no * being explicit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cdisutilstest.code.conftest import *
+from cdisutilstest.code.conftest import indexd_client, mock_server, indexd_server
 
 @pytest.fixture(scope='function')
 def index_client(indexd_client):
