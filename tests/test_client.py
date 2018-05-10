619ffcbf3bf5393f8343e31163b1462913776604
Rowland Ogwara <rogwara@uchicago.edu>
2018-05-10 13:29:57 -0500

    refactored to use util functions from cdisutils-test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,6 @@
-import uuid
-
 import pytest
 from requests import HTTPError
-
-from indexclient.client import Document
+from cdisutilstest.code.indexd_fixture import create_random_index, create_random_index_version
 
 
 def test_instantiate(index_client):
@@ -28,8 +25,8 @@ def test_instantiate(index_client):
 
 
 def test_create_with_metadata(index_client):
-    urls = ['s3://bucket/key']
-    urls_metadata = {'s3://bucket/key': {'k': 'v'}}
+    urls = ['s3://s3-host.com/bucket/key']
+    urls_metadata = {'s3://s3-host.com/bucket/key': {'k': 'v'}}
     size = 5
     acl = ['a', 'b']
     hashes = {'md5': 'ab167e49d25b488939b1ede42752458b'}
@@ -48,16 +45,9 @@ def test_create_with_metadata(index_client):
 
 def test_list_with_params(index_client):
     hashes = {'md5': 'ab167e49d25b488939b1ede42752458c'}
-    doc1 = index_client.create(
-        hashes=hashes,
-        size=1,
-        urls=[]
-    )
-    doc2 = index_client.create(
-        hashes=hashes,
-        size=1,
-        urls=[]
-    )
+    doc1 = create_random_index(index_client, hashes=hashes)
+    doc2 = create_random_index(index_client, hashes=hashes)
+
     docs_with_hashes = index_client.list_with_params(
         page_size=1,
         params={'hashes': hashes}
@@ -73,67 +63,22 @@ def test_list_with_params(index_client):
 def test_get_latest_version(index_client):
     """
     Args:
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
-    hashes = {'md5': 'ab167e49d25b488939b1ede42752458c'}
-    doc = index_client.create(
-        hashes=hashes,
-        size=10,
-        file_name="removable.txt",
-        urls=["s3://service.hidden.us/lostinspace"]
-    )
+    doc = create_random_index(index_client)
     latest = index_client.get_latest_version(doc.did)
     assert latest.did == doc.did
     assert latest.file_name == doc.file_name
     assert latest.hashes == doc.hashes
 
 
-def create_test_index(index_client, version=None, current_did=None):
-    """
-        Shorthand for creating new index entries for test purposes
-        Args:
-            index_client (indexclient.client.IndexClient):
-            version (str): version number of the index should be added
-            current_did (str): if specifed add a version of the index with the did = current_did
-        Returns:
-            indexclient.client.Document: versioned document
-        """
-
-    did = str(uuid.uuid4())
-
-    # add a new node index
-    hashes = {'md5': u'ab167e49d25b488939b1ede42752458c'}
-
-    if current_did:
-        data = {
-            "size": 10,
-            "hashes": hashes,
-            "urls": [],
-            "form": "object",
-            "file_name": did + "_super_indexed_razzmatazz.xtx"
-        }
-        if version:
-            data["version"] = version
-        doc = index_client.add_version(current_did, Document(None, None, data))
-    else:
-        doc = index_client.create(
-            hashes=hashes,
-            size=5,
-            version=version if version else "",
-            file_name=did + "_super_indexed_razzmatazz.xtx",
-            urls=[]
-        )
-
-    return doc
-
-
 def test_get_latest_version_with_skip(index_client):
     """
     Args:
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
-    doc = create_test_index(index_client, version="1")
-    doc_2 = create_test_index(index_client, current_did=doc.did)
+    doc = create_random_index(index_client, version="1")
+    doc_2 = create_random_index_version(index_client, did=doc.did)
 
     v_doc = index_client.get_latest_version(doc_2.did, skip_null_versions=True)
     assert v_doc.did == doc.did
@@ -147,7 +92,7 @@ def test_invalid_input(arg, exception, index_client):
     Args:
         arg(str): uuid
         exception (Exception): Exception class
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
 
     with pytest.raises(exception):
@@ -157,26 +102,11 @@ def test_invalid_input(arg, exception, index_client):
 def test_add_version(index_client):
     """
     Args:
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
 
-    hashes = {'md5': 'ab167e49d25b488939b1ede42752458c'}
-    doc = index_client.create(
-        hashes=hashes,
-        size=1,
-        urls=[]
-    )
-
-    data = {
-        k: v for k, v in doc._doc.items()
-        if k in ['size', 'hashes', 'form', 'urls']
-    }
-    data['version'] = '1'
-    new_doc = Document(
-        None, None, data
-    )
-
-    rev_doc = index_client.add_version(doc.did, new_doc)
+    doc = create_random_index(index_client)
+    rev_doc = create_random_index_version(index_client, doc.did, version="1")
     assert rev_doc.did is not doc.did
     assert rev_doc.baseid == doc.baseid
     assert rev_doc.version == "1"
@@ -189,47 +119,25 @@ def test_add_version(index_client):
 def test_list_versions(index_client):
     """
     Args:
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
-    hashes = {'md5': 'ab167e49d25b488939b1ede42752458c'}
-    doc = index_client.create(
-        hashes=hashes,
-        size=12,
-        file_name="brutalsheep.txt",
-        urls=["s3://service.hidden.us/foundalsoinspace"]
-    )
+    doc = create_random_index(index_client)
 
     # add a version
-
-    data = {
-        k: v for k, v in doc._doc.items()
-        if k in ['size', 'hashes', 'form', 'urls']
-    }
-    data['version'] = '1'
-    new_doc = Document(
-        None, None, data
-    )
-    rev_doc = index_client.add_version(doc.did, new_doc)
+    rev_doc = create_random_index_version(index_client, did=doc.did, version="1")
     assert rev_doc is not None
 
     # list versions
     versions = index_client.list_versions(doc.did)
-
     assert len(versions) == 2
 
 
 def test_updating_metadata(index_client):
     """
     Args:
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
-    hashes = {'md5': 'ab167e49d25b488939b1ede42752458c'}
-    doc = index_client.create(
-        hashes=hashes,
-        size=12,
-        file_name="brutalsheep.txt",
-        urls=["s3://service.hidden.us/foundalsoinspace"]
-    )
+    doc = create_random_index(index_client)
 
     doc.metadata["dummy_field"] = "Dummy Var"
     doc.urls_metadata[doc.urls[0]] = {'a': 'b'}
@@ -244,14 +152,9 @@ def test_updating_metadata(index_client):
 def test_updating_acl(index_client):
     """
     Args:
-        index_client (indexclient.client.IndexClient): injected index client
+        index_client (indexclient.client.IndexClient): IndexClient Pytest Fixture
     """
-    hashes = {'md5': 'ab167e49d25b488939b1ede42752458c'}
-    doc = index_client.create(
-        hashes=hashes,
-        size=12,
-        urls=["s3://service.hidden.us/foundalsoinspace"]
-    )
+    doc = create_random_index(index_client)
 
     doc.acl = ['a']
     doc.patch()
