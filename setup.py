f8298da969f7150fc87008c0b0c9322950f14802
Alexander VanTol <Avantol13@users.noreply.github.com>
2019-08-14 14:14:43 -0500

    chore(setup): change to new version to release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="indexclient",
-    version="1.5.2",
+    version="1.6.1",
     packages=["indexclient", "indexclient.parsers"],
     install_requires=["requests>=2.5.2,<3.0.0"],
 )
