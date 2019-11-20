161f05b82d8b6dda8fdfe00c9bda5e1d18335757
Michael Ingram <mpingram@uchicago.edu>
2019-11-20 15:53:37 -0600

    Bump indexclient to 2.0.0; drop py2 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="indexclient",
-    version="1.6.1",
+    version="2.0.0",
     packages=["indexclient", "indexclient.parsers"],
     install_requires=["requests>=2.5.2,<3.0.0"],
 )
