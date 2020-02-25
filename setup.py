cd0676877a88967a35cf279d8ee790e8f06d13ea
Alexander VanTol <Avantol13@users.noreply.github.com>
2020-02-25 10:58:53 -0600

    feat(setup.py): get version automatically

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+from subprocess import check_output
+
 from setuptools import setup
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 setup(
     name="indexclient",
-    version="1.6.2",
+    version=get_version(),
     packages=["indexclient", "indexclient.parsers"],
     install_requires=["requests>=2.5.2,<3.0.0"],
 )
