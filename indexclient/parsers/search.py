e970c4563922d52efd077a1adead8a0ed0a62004
George Thomas <george42@uchicago.edu>
2025-01-29 13:00:34 -0800

    Specify 'from err'

--- indexclient/parsers/search.py
+++ indexclient/parsers/search.py
@@ -101,13 +101,13 @@ def search_names(host, port, limit, start, size, hashes, **kwargs):
     try:
         res.raise_for_status()
     except Exception as err:
-        raise errors.BaseIndexError(res.status_code, res.text)
+        raise errors.BaseIndexError(res.status_code, res.text) from err
 
     try:
         doc = res.json()
     except ValueError as err:
         reason = json.dumps({"error": "invalid json payload returned"})
-        raise errors.BaseIndexError(res.status_code, reason)
+        raise errors.BaseIndexError(res.status_code, reason) from err
 
     sys.stdout.write(json.dumps(doc))
 
